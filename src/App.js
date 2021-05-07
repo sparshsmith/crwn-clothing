49db902f88179ba0ca40b10b1d6fcec1c66d4846
sparshsmith <sparshsmith@outlook.com>
2021-05-08 00:52:51 +0530

    Added header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/HomePage';
 import Shop from './pages/shop/Shop';
+import Header from './components/header/Header'
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
   }
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={Shop} />
